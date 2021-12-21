d851f1f58c008418f4f3309c1da4f1775d780984
ztfes <zach.github@mailbox.org>
2021-12-20 19:17:19 -0600

    2nd session with easy-1 problem set

--- easy-1/reverse_it_p2.rb
+++ easy-1/reverse_it_p2.rb
@@ -43,14 +43,3 @@ ________________________________________________________________________________
 =end
 
 ###### Code ######
-
-def reverse_sentence(string)
-  string.split(' ').reverse.join(' ')
-end
-
-# puts reverse_sentence('Hello World')
-
-puts reverse_sentence('Hello World') == 'World Hello'
-puts reverse_sentence('Reverse these words') == 'words these Reverse'
-puts reverse_sentence('') == ''
-puts reverse_sentence('    ') == '' # Any number of spaces results in ''
