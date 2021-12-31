163812d604a71fb3f7b86d2d9be756e168830233
ztfes <zach.github@mailbox.org>
2021-12-30 20:47:39 -0600

    Started RPS bonus features in lesson 2. Completed 1st exercise in RB101/109 problem set "Easy 3"

--- lesson_2_small_programs/RPS_bonus.rb
+++ lesson_2_small_programs/RPS_bonus.rb
@@ -1,11 +1,15 @@
-# In this assignment, we'll build a Rock Paper Scissors game.
-# The game flow should go like this:
+# NEW FEATURES #
 
-#    - the user makes a choice
-#    - the computer makes a choice
-#    - the winner is displayed
+# 1) Add 'lizard' and 'spock' to VALID_CHOICES
+# 1a) Add additional game logic for inclusion of 'lizard' and 'spock'
+# 2) Simplify user-input for 'choice': Allow for abbreviated input
+# 3) Make game logiic a 'first of three' to win
+# 3a) Need a counter to track Player_wins and computer_wins
+# 3b) Exit when player_wins OR computer_wins reaches 3
+# 3c) Add prompt to state who won
+# 4) Ask user if they'd like to play again
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 def prompt(message)
   Kernel.puts("=> #{message}")
@@ -13,8 +17,15 @@ end
 
 def win?(first, second)
   (first == 'rock' && second == 'scissors') ||
+    (first == 'rock' && second == 'lizard') ||
     (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+    (first == 'paper' && second == 'spock') ||
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'spock' && second == 'rock') ||
+    (first == 'spock' && second == 'scissors')
 end
 
 def display_results(player, computer)
