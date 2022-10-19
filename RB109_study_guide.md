add90367c0131f6485a70e0a8ba8471655da55d2
ztfes <zach.github@mailbox.org>
2022-10-18 20:16:00 -0500

    redoing small-problems set for RB101-109, combined RB101 and RB109 folders

---
+++ RB109_study_guide.md
@@ -0,0 +1,29 @@
+<details><summary>**Variable Scope**</summary>
+
+<details><summary>**(non)Mutating Methods**</summary>
+
+<details><summary>**Pass-By-Reference vs. Pass-By-Value**</summary>
+
+<details><summary>**Collections**</summary>
+
+<details><summary>**Variables As Pointers**</summary>
+
+<details><summary>**Puts vs. Returns**</summary>
+
+<details><summary>**False, Nil, & "Truthiness"**</summary>
+
+<details><summary>**Method Definition vs. Invocation**</summary>
+
+<details><summary>**Implicit Return Values of Method Invocations and Blocks**</summary>
+
+
+
+    local variable scope, especially how local variables interact with method invocations with blocks and method definitions
+    mutating vs non-mutating methods, pass-by-reference vs pass-by-value
+    working with collections (Array, Hash, String), and popular collection methods (each, map, select, etc). Review the two lessons on these topics thoroughly.
+    variables as pointers
+    puts vs return
+    false vs nil and the idea of "truthiness"
+    method definition and method invocation
+    implicit return value of method invocations and blocks
+    how the Array#sort method works
--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -2,82 +2,168 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-WINNING_TOTAL = 21
-DEALER_HIT_LIMIT = 17
-
-full_deck = [['H', 2], ['H', 3], ['H', 4], ['H', 5], ['H', 6],
-             ['H', 7], ['H', 8], ['H', 9], ['H', 10], ['H', 'J'],
-             ['H', 'Q'], ['H', 'K'], ['H', 'A'],
-             ['D', 2], ['D', 3], ['D', 4], ['D', 5], ['D', 6],
-             ['D', 7], ['D', 8], ['D', 9], ['D', 10], ['D', 'J'],
-             ['D', 'Q'], ['D', 'K'], ['D', 'A'],
-             ['C', 2], ['C', 3], ['C', 4], ['C', 5], ['C', 6],
-             ['C', 7], ['C', 8], ['C', 9], ['C', 10], ['C', 'J'],
-             ['C', 'Q'], ['C', 'K'], ['C', 'A'],
-             ['S', 2], ['S', 3], ['S', 4], ['S', 5], ['S', 6],
-             ['S', 7], ['S', 8], ['S', 9], ['S', 10], ['S', 'J'],
-             ['S', 'Q'], ['S', 'K'], ['S', 'A']]
-
 def deal_starting_hand(deck)
   deck.shuffle!.pop(2)
 end
 
 def calculate_hand(hand)
-  total = []
-  hand.each do |card|
-    if card[1].class == String && card[1] != 'A'
-      total << 10
-    elsif card[1].class == String && card[1] == 'A'
-      total << ace_value(total)
+  values = hand.map { |card| card[1] }
+
+  total = 0
+  values.each do |value|
+    if value == "A"
+      total += 11
+    elsif value.to_i == 0
+      total += 10
     else
-      total << card[1]
+      total += value.to_i
     end
   end
-  total.sum
-end
 
-def ace_value(total)
-  card_value = nil
-  if total.sum + 11 < WINNING_TOTAL
-    card_value = 11
-  else
-    card_value = 1
+  values.select { |value| value == "A" }.count.times do
+    total -= 10 if total > WINNING_SCORE
   end
-  card_value
+
+  total
 end
 
 def display_hand(user, hand, total)
-  prompt "#{user} has: #{hand}, Total = #{total}"
+  if user == 'Dealer'
+    prompt "#{user} has: #{hand[0]} and unknown card(s)"
+  else
+    prompt "#{user} have: #{hand}, Total = #{total}"
+  end
 end
 
-def dealer_turn
-  # dealer hits until at least 17
-  # if total >= 17 ---> STAY
-  # else HIT
+def dealer_winning_hand(user, hand, total) # display dealers hand if they win
+  prompt "#{user} had: #{hand}, Total = #{total}"
 end
 
-def player_turn
-  # display your current score
-  # ask if you want to hit (prompt, chomp)
-  # if HIT, shuffle-pop card to hand
-  # display new score
+def busted?(total)
+  total > WINNING_SCORE
 end
 
-def hit_or_stay
-  loop do
+def who_won?(player_total, dealer_total)
+  if (player_total > dealer_total) && !busted?(player_total)
+    'Player'
+  elsif (dealer_total > player_total) && !busted?(dealer_total)
+    'Dealer'
+  elsif busted?(player_total)
+    'Player Busted'
+  elsif busted?(dealer_total)
+    'Dealer Busted'
+  else
+    'Tie'
+  end
 end
 
-def bust?
+loop do
+  # Refresh the deck with each game
+  full_deck = [['H', 2], ['H', 3], ['H', 4], ['H', 5], ['H', 6],
+               ['H', 7], ['H', 8], ['H', 9], ['H', 10], ['H', 'J'],
+               ['H', 'Q'], ['H', 'K'], ['H', 'A'],
+               ['D', 2], ['D', 3], ['D', 4], ['D', 5], ['D', 6],
+               ['D', 7], ['D', 8], ['D', 9], ['D', 10], ['D', 'J'],
+               ['D', 'Q'], ['D', 'K'], ['D', 'A'],
+               ['C', 2], ['C', 3], ['C', 4], ['C', 5], ['C', 6],
+               ['C', 7], ['C', 8], ['C', 9], ['C', 10], ['C', 'J'],
+               ['C', 'Q'], ['C', 'K'], ['C', 'A'],
+               ['S', 2], ['S', 3], ['S', 4], ['S', 5], ['S', 6],
+               ['S', 7], ['S', 8], ['S', 9], ['S', 10], ['S', 'J'],
+               ['S', 'Q'], ['S', 'K'], ['S', 'A']]
+
+  # Keep track of winning rounds
+  player_score = 0
+  dealer_score = 0
+
+  # Set custom winning score'
+  prompt "Please set the target winning total (e.g., 21):"
+  WINNING_SCORE = gets.chomp.to_i
+  DEALER_HIT_LIMIT = WINNING_SCORE - 4
+
+  loop do # main loop
+    system 'clear'
+    prompt "Welcome to the game, you have chose to play to #{WINNING_SCORE}"
+    prompt "First to 5 wins!"
+    prompt "Player: #{player_score} | Dealer: #{dealer_score}"
+
+    player_hand = deal_starting_hand(full_deck)
+    dealer_hand = deal_starting_hand(full_deck)
+
+    player_total = calculate_hand(player_hand)
+    dealer_total = calculate_hand(dealer_hand)
+
+    display_hand('You', player_hand, player_total)
+    display_hand('Dealer', dealer_hand, dealer_total)
+
+    prompt "Player will start:"
+    loop do
+      loop do # Player Turn
+        prompt 'Would you like to hit or stay?'
+        answer = gets.chomp
+        break unless answer.downcase.start_with?('h')
+        player_hand << full_deck.shuffle!.pop
+        player_total = calculate_hand(player_hand)
+        display_hand('You', player_hand, player_total)
+        break if busted?(player_total)
+      end
+
+      break if busted?(player_total)
+      prompt "You chose to stay."
+      display_hand('You', player_hand, player_total)
+
+      prompt "It is now the dealer's turn"
+
+      loop do # Dealer Turn
+        break if dealer_total >= DEALER_HIT_LIMIT
+        break if busted?(dealer_total)
+        dealer_hand << full_deck.shuffle!.pop
+        prompt "Dealer hit"
+        dealer_total = calculate_hand(dealer_hand)
+      end
+
+      if busted?(dealer_total)
+        break
+      else
+        prompt "Dealer chose to stay."
+        break
+      end
+    end
+
+    case who_won?(player_total, dealer_total)
+    when 'Player'
+      prompt "Player wins!"
+      player_score += 1
+    when 'Dealer'
+      prompt "Dealer wins!"
+      dealer_score += 1
+    when 'Player Busted'
+      prompt "You busted, dealer wins!"
+      dealer_score += 1
+    when 'Dealer Busted'
+      prompt "Dealer busted, you win!"
+      player_score += 1
+    else
+      prompt "It's a tie!"
+    end
+
+    dealer_winning_hand('Dealer', dealer_hand, dealer_total)
+
+    if player_score == 5
+      prompt "Player: #{player_score} | Dealer: #{dealer_score}"
+      prompt "You win the game!"
+      break
+    elsif dealer_score == 5
+      prompt "Player: #{player_score} | Dealer: #{dealer_score}"
+      prompt "Dealer won the game!"
+      break
+    end
+
+    prompt "Hit any key to start next round"
+    gets
+  end
+  prompt "Would you like to play another game of #{WINNING_SCORE}? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
-
-def winner?
-end
-
-player_hand = []
-dealer_hand = []
-loop do #main loop
-
-### TEST CASE ###
-# player_hand = calculate_hand(full_deck)
-# player_total = calculate_hand(player_hand)
-# display_hand('player', player_hand, player_total)
+prompt "Thanks for playing #{WINNING_SCORE}!"
